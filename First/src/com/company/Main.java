9238e94d90e22760b506f2fd6c41845646b43420
Dima Voznesenskii <Simasorokin@gmail.com>
2017-02-15 23:58:43 +0300

    Получение числа людей и каждому через год добавлять +1 год к возрасту.

--- First/src/com/company/Main.java
+++ First/src/com/company/Main.java
@@ -1,8 +1,34 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+	    // нам надо получить пользователей
+        int n;
+        do {
+            System.out.println("Получить число:");
+            n = sc.nextInt();
+        }
+        while (n < 1);
+        System.out.println("Вы ввели верное число!");
+
+        int ages[n];
+
+        for (int i = 0; i < n; i++);
+        {
+            System.out.println("sdfsdfsd", i+1);
+            ages[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++);
+        {
+            System.out.println("возраст :" + (n+1) + (ages[n]+1));
+
+        }
+
     }
+
 }
